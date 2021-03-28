7660e1dd72244d44a2f277fc7e5c604d52cdfd51
Muhammad Bilal <bilal.se74@gmail.com>
2021-03-28 10:09:24 +0500

    Added MyNetwork player script and Sync the Variables

--- MultiplayerBasics/Assets/Scripts/MyNetworkManager.cs
+++ MultiplayerBasics/Assets/Scripts/MyNetworkManager.cs
@@ -5,17 +5,16 @@ using UnityEngine;
 
 public class MyNetworkManager : NetworkManager
 {
-    public override void OnClientConnect(NetworkConnection conn)
-    {
-        base.OnClientConnect(conn);
-
-        Debug.Log("I connected to a server!");
-    }
+    
 
     public override void OnServerAddPlayer(NetworkConnection conn)
     {
         base.OnServerAddPlayer(conn);
 
-        Debug.Log($"There are now {numPlayers} players");
+        MyNetworkPlayer player = conn.identity.GetComponent<MyNetworkPlayer>();
+
+        player.SetDiplayName($"Player {numPlayers}");
+        player.setColor();
+
     }
 }
---
+++ MultiplayerBasics/Assets/Scripts/MyNetworkPlayer.cs
@@ -0,0 +1,28 @@
+﻿using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyNetworkPlayer : NetworkBehaviour
+{
+    [SyncVar]
+    [SerializeField]
+    private string diplayName = "Missing Name";
+
+    [SyncVar]
+    [SerializeField]
+    Color color;
+
+    [Server]
+    public void setColor()
+    {
+        color = new Color(Random.Range(0f,1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+    }
+
+
+    [Server]
+    public void SetDiplayName(string _newDisplayName)
+    {
+        diplayName = _newDisplayName;
+    }
+}
